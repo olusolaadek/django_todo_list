aa9aa2746b9c77936ebdbd59ead138dd75547d62
OLUSOLA ADEKUNLE <olusola.adekunle@outlook.com>
2022-03-19 17:15:44 +0100

    Add Delete routes for list and items

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import (
-    ListView, CreateView, UpdateView
+    ListView, CreateView, UpdateView, DeleteView,
 )
 
 from .models import ToDoItem, ToDoList
@@ -78,3 +78,24 @@ class ItemUpdate(UpdateView):
 
     def get_success_url(self):
         return reverse("list", args=[self.object.todo_list_id])
+
+# todo_list/todo_app/views.py
+
+
+class ListDelete(DeleteView):
+    model = ToDoList
+    # You have to use reverse_lazy() instead of reverse(),
+    # as the urls are not loaded when the file is imported.
+    success_url = reverse_lazy("index")
+
+
+class ItemDelete(DeleteView):
+    model = ToDoItem
+
+    def get_success_url(self):
+        return reverse_lazy("list", args=[self.kwargs["list_id"]])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["todo_list"] = self.object.todo_list
+        return context
